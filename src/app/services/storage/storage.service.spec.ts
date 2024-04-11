92d210b79a4e56173b06226d37e00cc6d0b59e6c
karmasakshi <karmasakshi@users.noreply.github.com>
2024-04-12 00:22:43 +0530

    test(general): add mocks

--- src/app/services/storage/storage.service.spec.ts
+++ src/app/services/storage/storage.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { getTranslocoModule } from '@xxx/modules/transloco-testing.module';
+import { LoggerService } from '../logger/logger.service';
+import { MockLoggerService } from '../logger/logger.service.mock';
 import { StorageService } from './storage.service';
 
 describe('StorageService', () => {
@@ -8,6 +10,7 @@ describe('StorageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [getTranslocoModule()],
+      providers: [{ provide: LoggerService, useClass: MockLoggerService }],
     });
 
     service = TestBed.inject(StorageService);
