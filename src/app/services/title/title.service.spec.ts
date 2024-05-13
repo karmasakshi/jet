063c48e19500ad57fc2b719881b23dc420808877
karmasakshi <karmasakshi@users.noreply.github.com>
2024-05-13 16:18:44 +0400

    style(general): clean-up

--- src/app/services/title/title.service.spec.ts
+++ src/app/services/title/title.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { getTranslocoModule } from '@jet/modules/transloco-testing.module';
 import { LoggerService } from '../logger/logger.service';
 import { MockLoggerService } from '../logger/logger.service.mock';
 import { TitleService } from './title.service';
@@ -8,6 +9,7 @@ describe('TitleService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [getTranslocoModule()],
       providers: [{ provide: LoggerService, useClass: MockLoggerService }],
     });
     service = TestBed.inject(TitleService);
