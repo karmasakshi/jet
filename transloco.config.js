a06969a1850d9a45188b5fc180d67b417a7c35d9
karmasakshi <karmasakshi@users.noreply.github.com>
2024-05-29 13:50:05 +0400

    refactor(general): clean-up

--- transloco.config.js
+++ transloco.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  rootTranslationsPath: '../public/assets/i18n',
+  rootTranslationsPath: '../public/assets/i18n/',
   langs: ['ar', 'en'],
   keysManager: {
-    output: '../public/assets/i18n',
+    output: '../public/assets/i18n/',
     sort: true,
     unflat: true,
   },
