42860277bc27c5c739cd72cb6c155454fec51af1
karmasakshi <karmasakshi@users.noreply.github.com>
2024-06-05 00:15:46 +0400

    refactor(general): clean-up

--- src/app/services/update/update.service.mock.ts
+++ src/app/services/update/update.service.mock.ts
@@ -1,10 +1,12 @@
-import { Subscription } from 'rxjs';
+import { Observable, Subscription, of } from 'rxjs';
 
 export class UpdateServiceMock {
   public readonly swUpdateSubscription: Subscription;
+  public readonly lastUpdateCheckTimestamp$: Observable<string>;
 
   public constructor() {
     this.swUpdateSubscription = Subscription.EMPTY;
+    this.lastUpdateCheckTimestamp$ = of('');
   }
   public checkForUpdate(): void {
     // Mock implementation, do nothing
