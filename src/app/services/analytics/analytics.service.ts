df8261f07236595223afd50aac2bfd6067201b76
karmasakshi <karmasakshi@users.noreply.github.com>
2024-06-09 02:11:54 +0400

    refactor(general): clean-up

--- src/app/services/analytics/analytics.service.ts
+++ src/app/services/analytics/analytics.service.ts
@@ -13,7 +13,7 @@ export class AnalyticsService {
     this._loggerService.logServiceInitialization('AnalyticsService');
   }
 
-  public track(eventName: string, eventData?: string): void {
+  public track(eventName: string, eventData?: unknown): void {
     if (this._isAnalyticsEnabled) {
       if (eventData) {
         this._loggerService.logMessages(eventName, eventData);
