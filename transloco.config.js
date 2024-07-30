58b83123aaf13a9f7256f8d4b0cc7b57232412c0
karmasakshi <karmasakshi@users.noreply.github.com>
2024-07-30 19:38:25 +0400

    build(general): update dependencies

--- transloco.config.js
+++ transloco.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  rootTranslationsPath: '../public/i18n/',
+  rootTranslationsPath: 'public/i18n/',
   langs: ['ar', 'en'],
   keysManager: {
-    output: '../public/i18n/',
+    output: 'public/i18n/',
     sort: true,
     unflat: true,
   },
