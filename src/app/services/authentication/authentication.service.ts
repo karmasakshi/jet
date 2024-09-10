b7591e56d63dd9f33c50d3c84d611c9d386494a5
karmasakshi <karmasakshi@users.noreply.github.com>
2024-09-10 18:31:53 +0530

    chore(general): clean-up

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -32,7 +32,7 @@ export class AuthenticationService {
         window.setTimeout(() => {
           this._user.set({});
           resolve();
-        }, 3000);
+        }, 1800);
       } catch (error) {
         this._loggerService.logError(error);
         reject(new Error());
@@ -46,7 +46,7 @@ export class AuthenticationService {
         window.setTimeout(() => {
           this._user.set(null);
           resolve();
-        }, 3000);
+        }, 1800);
       } catch (error) {
         this._loggerService.logError(error);
         reject(new Error());
