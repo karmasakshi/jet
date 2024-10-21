70c0a9a15097f6cd5e1978ae33861c47ac8c8540
karmasakshi <karmasakshi@users.noreply.github.com>
2024-10-21 21:02:01 +0530

    build(general): add transloco-optimize

--- transloco.config.ts
+++ transloco.config.ts
@@ -1,6 +1,10 @@
+import { isDevMode } from '@angular/core';
 import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
 
 const config: TranslocoGlobalConfig = {
+  flatten: {
+    aot: !isDevMode(),
+  },
   keysManager: {
     output: 'public/i18n/',
     sort: true,
