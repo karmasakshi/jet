736bf42676a5b5390c9c72a3605b9ffcbfa61632
karmasakshi <karmasakshi@users.noreply.github.com>
2024-12-18 15:07:28 +0400

    chore(general): clean-up

--- src/app/services/supabase/supabase.service.ts
+++ src/app/services/supabase/supabase.service.ts
@@ -12,8 +12,8 @@ export class SupabaseService {
 
   public constructor() {
     this._supabaseClient = createClient(
-      import.meta.env.NG_APP_SUPABASE_URL ?? '',
-      import.meta.env.NG_APP_SUPABASE_KEY ?? '',
+      import.meta.env.NG_APP_SUPABASE_URL,
+      import.meta.env.NG_APP_SUPABASE_KEY,
     );
 
     this._loggerService.logServiceInitialization('SupabaseService');
