93b36791a3d7f82ee226cb1258615b395a798144
karmasakshi <karmasakshi@users.noreply.github.com>
2024-12-18 18:05:19 +0400

    chore(general): clean-up

--- src/app/services/supabase/supabase.service.mock.ts
+++ src/app/services/supabase/supabase.service.mock.ts
@@ -4,7 +4,10 @@ export class SupabaseService {
   private readonly _supabaseClient: SupabaseClient;
 
   public constructor() {
-    this._supabaseClient = createClient('', '');
+    this._supabaseClient = createClient(
+      import.meta.env.NG_APP_SUPABASE_URL,
+      import.meta.env.NG_APP_SUPABASE_KEY,
+    );
   }
 
   public get supabaseClient(): SupabaseClient {
