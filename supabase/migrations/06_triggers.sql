34f30acfe03308b211fb65f33a29fc96d413b678
karmasakshi <karmasakshi@users.noreply.github.com>
2025-05-13 21:42:32 +0400

    chore(general): clean-up

--- supabase/migrations/06_triggers.sql
+++ supabase/migrations/06_triggers.sql
@@ -2,10 +2,10 @@
 
 create or replace trigger insert_profile
 after insert on auth.users for each row
-execute function insert_profile ();
+execute function insert_profile();
 
 -- public.profiles
 
-create or replace trigger update_updated_at before
-update on public.profiles for each row
-execute function update_updated_at ();
+create or replace trigger update_updated_at
+before update on public.profiles for each row
+execute function update_updated_at();
