f18dc87ad756589d20d204d0b05fac6dd4a869f1
karmasakshi <karmasakshi@users.noreply.github.com>
2025-06-08 22:27:11 +0400

    chore(general): clean-up

--- eslint.config.js
+++ eslint.config.js
@@ -22,8 +22,6 @@ module.exports = tseslint.config(
         'error',
         { type: 'element', prefix: 'jet', style: 'kebab-case' },
       ],
-      '@angular-eslint/component-class-suffix': 'off',
-      '@angular-eslint/directive-class-suffix': 'off',
       '@typescript-eslint/explicit-member-accessibility': 'error',
       '@typescript-eslint/no-inferrable-types': 'off',
       'sort-keys': ['error', 'asc', { caseSensitive: false }],
