43079206e90db5a3704230bb1c762fbaca30d9f7
karmasakshi <karmasakshi@users.noreply.github.com>
2025-08-29 11:10:58 +0400

    chore(general): clean-up

--- supabase/migrations/11_rbac.sql
+++ supabase/migrations/11_rbac.sql
@@ -8,7 +8,7 @@ create type public.role as enum ('admin');
 
 create table public.permissions (
   id uuid primary key default gen_random_uuid(),
-  name text not null unique,
+  permission text not null unique,
   created_at timestamptz not null default now(),
   updated_at timestamptz not null default now()
 );
@@ -36,7 +36,7 @@ alter table public.role_permissions enable row level security;
 
 -- security invoker
 
-create or replace function public.set_name()
+create or replace function public.set_permission()
 returns trigger
 language plpgsql
 security invoker
@@ -54,8 +54,8 @@ begin
     return new;
   end if;
 
-  raise warning 'Cannot modify % in %.%', 'name', TG_TABLE_SCHEMA, TG_TABLE_NAME;
-  new.name := old.name;
+  raise warning 'Cannot modify % in %.%', 'permission', TG_TABLE_SCHEMA, TG_TABLE_NAME;
+  new.permission := old.permission;
 
   return new;
 end;
@@ -90,11 +90,11 @@ $$;
 
 -- public.permissions
 
-create or replace trigger set_name
+create or replace trigger set_permission
 before update
 on public.permissions
 for each row
-execute function public.set_name();
+execute function public.set_permission();
 
 create or replace trigger set_created_at
 before update
@@ -132,7 +132,7 @@ execute procedure moddatetime(updated_at);
 
 -- seed
 
-insert into public.permissions (name)
+insert into public.permissions (permission)
 values ('profiles.select'), ('profiles.update'), ('profiles.delete');
 
 -- auth hook
@@ -151,11 +151,11 @@ begin
 
   claims := event->'claims';
 
-  if _role is null then
-    claims := jsonb_set(claims, '{role}', 'null');
-  else
-    claims := jsonb_set(claims, '{role}', to_jsonb(_role));
+  if jsonb_typeof(claims->'app_metadata') is null then
+    claims := jsonb_set(claims, '{app_metadata}', '{}');
   end if;
+
+  claims := jsonb_set(claims, '{app_metadata, role}', _role);
 
   event := jsonb_set(event, '{claims}', claims);
 
@@ -163,6 +163,6 @@ begin
 end;
 $$;
 
-grant execute on function public.custom_access_token_hook to supabase_auth_admin;
+grant all on table public.profiles to supabase_auth_admin;
 
-revoke execute on function public.custom_access_token_hook from authenticated, anon, public;
+revoke all on table public.profiles from authenticated, anon, public;
