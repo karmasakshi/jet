0fda54bd528a4ed6b0497aa57c4d72922ad47b5e
karmasakshi <karmasakshi@users.noreply.github.com>
2025-12-02 10:20:50 +0400

    chore(general): clean-up

--- eslint.config.js
+++ eslint.config.js
@@ -5,111 +5,6 @@ const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 const perfectionist = require('eslint-plugin-perfectionist');
 
-const modernAngularRules = {
-  '@angular-eslint/prefer-inject': 'error',
-  '@angular-eslint/prefer-on-push-component-change-detection': 'error',
-  '@angular-eslint/prefer-signals': 'error',
-};
-
-const classRules = {
-  '@typescript-eslint/explicit-member-accessibility': 'error',
-  '@typescript-eslint/member-ordering': [
-    'error',
-    {
-      classes: [
-        '#private-instance-field',
-        'public-instance-field',
-        'constructor',
-        'public-instance-method',
-        '#private-instance-method',
-      ],
-    },
-  ],
-  '@typescript-eslint/prefer-readonly': 'error',
-};
-
-const namingRules = {
-  '@typescript-eslint/naming-convention': [
-    'error',
-    { selector: 'default', format: ['camelCase'], leadingUnderscore: 'forbid' },
-    {
-      selector: 'enumMember',
-      format: ['PascalCase'],
-      leadingUnderscore: 'forbid',
-    },
-    {
-      selector: 'method',
-      modifiers: ['private'],
-      format: ['camelCase'],
-      leadingUnderscore: 'require',
-    },
-    {
-      selector: 'parameter',
-      modifiers: ['unused'],
-      format: ['camelCase'],
-      leadingUnderscore: 'require',
-    },
-    {
-      selector: 'parameterProperty',
-      modifiers: ['private'],
-      format: ['camelCase'],
-      leadingUnderscore: 'require',
-    },
-    {
-      selector: 'property',
-      modifiers: ['private'],
-      format: ['camelCase', 'snake_case', 'UPPER_CASE'],
-      leadingUnderscore: 'require',
-    },
-    {
-      selector: 'property',
-      format: ['camelCase', 'snake_case', 'UPPER_CASE'],
-      leadingUnderscore: 'forbid',
-    },
-    {
-      selector: 'typeLike',
-      format: ['PascalCase'],
-      leadingUnderscore: 'forbid',
-    },
-    {
-      selector: 'variable',
-      modifiers: ['const'],
-      types: ['function'],
-      format: ['camelCase'],
-      leadingUnderscore: 'forbid',
-    },
-    {
-      selector: 'variable',
-      modifiers: ['const'],
-      format: ['camelCase', 'UPPER_CASE'],
-      leadingUnderscore: 'forbid',
-    },
-  ],
-};
-
-const perfectionistRules = {
-  'perfectionist/sort-enums': 'warn',
-  'perfectionist/sort-interfaces': 'warn',
-  'perfectionist/sort-intersection-types': 'warn',
-  'perfectionist/sort-objects': 'warn',
-  'perfectionist/sort-object-types': 'warn',
-  'perfectionist/sort-switch-case': 'warn',
-  'perfectionist/sort-union-types': 'warn',
-};
-
-const templateRules = {
-  '@angular-eslint/template/attributes-order': 'warn',
-  '@angular-eslint/template/cyclomatic-complexity': 'off',
-  '@angular-eslint/template/i18n': 'off',
-  '@angular-eslint/template/no-call-expression': 'off',
-  '@angular-eslint/template/no-inline-styles': 'off',
-  'no-restricted-syntax': [
-    'error',
-    { selector: 'BoundAttribute[name="ngClass"]' },
-    { selector: 'BoundAttribute[name="ngStyle"]' },
-  ],
-};
-
 module.exports = defineConfig([
   {
     files: ['**/*.ts'],
@@ -120,7 +15,7 @@ module.exports = defineConfig([
       angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
-    rules: /** @type {any} */ ({
+    rules: {
       '@angular-eslint/directive-selector': [
         'error',
         { type: 'attribute', prefix: 'jet', style: 'camelCase' },
@@ -129,9 +24,83 @@ module.exports = defineConfig([
         'error',
         { type: 'element', prefix: 'jet', style: 'kebab-case' },
       ],
-      ...modernAngularRules,
-      ...classRules,
-      ...namingRules,
+      '@angular-eslint/prefer-inject': 'error',
+      '@angular-eslint/prefer-on-push-component-change-detection': 'error',
+      '@angular-eslint/prefer-signals': 'error',
+      '@typescript-eslint/explicit-member-accessibility': 'error',
+      '@typescript-eslint/member-ordering': [
+        'error',
+        {
+          classes: [
+            '#private-instance-field',
+            'public-instance-field',
+            'constructor',
+            'public-instance-method',
+            '#private-instance-method',
+          ],
+        },
+      ],
+      '@typescript-eslint/prefer-readonly': 'error',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['camelCase'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: 'enumMember',
+          format: ['PascalCase'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: 'method',
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require',
+        },
+        {
+          selector: 'parameter',
+          modifiers: ['unused'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require',
+        },
+        {
+          selector: 'parameterProperty',
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require',
+        },
+        {
+          selector: 'property',
+          modifiers: ['private'],
+          format: ['camelCase', 'snake_case', 'UPPER_CASE'],
+          leadingUnderscore: 'require',
+        },
+        {
+          selector: 'property',
+          format: ['camelCase', 'snake_case', 'UPPER_CASE'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: 'variable',
+          modifiers: ['const'],
+          types: ['function'],
+          format: ['camelCase'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: 'variable',
+          modifiers: ['const'],
+          format: ['camelCase', 'UPPER_CASE'],
+          leadingUnderscore: 'forbid',
+        },
+      ],
       '@typescript-eslint/no-floating-promises': 'error',
       '@typescript-eslint/no-inferrable-types': 'off',
       'no-restricted-syntax': [
@@ -142,11 +111,17 @@ module.exports = defineConfig([
           message: 'Use ECMAScript private (#) instead of TypeScript private.',
         },
       ],
-      ...perfectionistRules,
+      'perfectionist/sort-enums': 'warn',
+      'perfectionist/sort-interfaces': 'warn',
+      'perfectionist/sort-intersection-types': 'warn',
+      'perfectionist/sort-objects': 'warn',
+      'perfectionist/sort-object-types': 'warn',
+      'perfectionist/sort-switch-case': 'warn',
+      'perfectionist/sort-union-types': 'warn',
       'no-alert': 'error',
       'no-console': 'error',
       'sort-keys': ['warn', 'asc', { caseSensitive: false }],
-    }),
+    },
     languageOptions: {
       parserOptions: {
         projectService: { allowDefaultProject: ['transloco.config.ts'] },
@@ -157,6 +132,17 @@ module.exports = defineConfig([
   {
     files: ['**/*.html'],
     extends: [angular.configs.templateAll],
-    rules: /** @type {any} */ (templateRules),
+    rules: {
+      '@angular-eslint/template/attributes-order': 'warn',
+      '@angular-eslint/template/cyclomatic-complexity': 'off',
+      '@angular-eslint/template/i18n': 'off',
+      '@angular-eslint/template/no-call-expression': 'off',
+      '@angular-eslint/template/no-inline-styles': 'off',
+      'no-restricted-syntax': [
+        'error',
+        { selector: 'BoundAttribute[name="ngClass"]' },
+        { selector: 'BoundAttribute[name="ngStyle"]' },
+      ],
+    },
   },
 ]);
