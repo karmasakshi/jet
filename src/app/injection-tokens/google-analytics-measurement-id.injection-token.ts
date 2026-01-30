3bdc542525c8ff9e1f66c2bf2013ba20bcd085e9
karmasakshi <karmasakshi@users.noreply.github.com>
2026-01-31 02:47:04 +0400

    chore(general): clean-up

--- src/app/injection-tokens/google-analytics-measurement-id.injection-token.ts
+++ src/app/injection-tokens/google-analytics-measurement-id.injection-token.ts
@@ -2,8 +2,6 @@ import { InjectionToken } from '@angular/core';
 
 export const GOOGLE_ANALYTICS_MEASUREMENT_ID: InjectionToken<string> =
   new InjectionToken<string>('GOOGLE_ANALYTICS_MEASUREMENT_ID', {
-    factory: () =>
-      import.meta.env?.NG_APP_GOOGLE_ANALYTICS_MEASUREMENT_ID ??
-      'your-google-analytics-measurement-id',
+    factory: () => import.meta.env.NG_APP_GOOGLE_ANALYTICS_MEASUREMENT_ID,
     providedIn: 'root',
   });
