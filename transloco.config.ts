ce262a1ab484c9f6299d678224a12a447fb52948
karmasakshi <karmasakshi@users.noreply.github.com>
2026-06-16 20:20:09 +0530

    chore(general): clean-up

--- transloco.config.ts
+++ transloco.config.ts
@@ -1,7 +1,7 @@
 import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
 
 export default {
-  keysManager: { output: 'public/i18n/', sort: true, unflat: true },
+  keysManager: { output: './public/i18n/', sort: true, unflat: true },
   langs: ['ar', 'en'],
-  rootTranslationsPath: 'public/i18n/',
+  rootTranslationsPath: './public/i18n/',
 } satisfies TranslocoGlobalConfig;
