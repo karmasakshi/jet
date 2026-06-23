e980a076341cf1e2ee48abe5d0d457d98dbdd270
karmasakshi <karmasakshi@users.noreply.github.com>
2026-06-23 21:12:58 +0530

    chore(general): re-add @ngx-env/builder

--- src/app/injection-tokens/google-analytics-measurement-id.injection-token.ts
+++ src/app/injection-tokens/google-analytics-measurement-id.injection-token.ts
@@ -2,5 +2,5 @@ import { InjectionToken } from '@angular/core';
 
 export const GOOGLE_ANALYTICS_MEASUREMENT_ID: InjectionToken<string> = new InjectionToken<string>(
   'GOOGLE_ANALYTICS_MEASUREMENT_ID',
-  { factory: () => import.meta.env.VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID, providedIn: 'root' },
+  { factory: () => import.meta.env.NG_APP_GOOGLE_ANALYTICS_MEASUREMENT_ID, providedIn: 'root' },
 );
---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+declare interface Env {
+  readonly HUSKY: string;
+  readonly NG_APP_GOOGLE_ANALYTICS_MEASUREMENT_ID: string;
+  readonly NG_APP_IS_ANALYTICS_ENABLED: string;
+  readonly NG_APP_IS_LOGGING_ENABLED: string;
+  readonly NG_APP_SUPABASE_PROJECT_URL: string;
+  readonly NG_APP_SUPABASE_PUBLISHABLE_KEY: string;
+}
+
+declare interface ImportMeta {
+  readonly env: Env;
+}
