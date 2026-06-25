d477a43a001d18b99bdcb70720aac985151b7585
karmasakshi <karmasakshi@users.noreply.github.com>
2026-06-25 14:04:06 +0530

    chore(general): clean-up

--- src/app/injection-tokens/supabase-client.injection-token.ts
+++ src/app/injection-tokens/supabase-client.injection-token.ts
@@ -6,8 +6,8 @@ export const SUPABASE_CLIENT: InjectionToken<SupabaseClient> = new InjectionToke
   {
     factory: () =>
       createClient(
-        import.meta.env.NG_APP_SUPABASE_PROJECT_URL,
-        import.meta.env.NG_APP_SUPABASE_PUBLISHABLE_KEY,
+        import.meta.env.NG_APP_SUPABASE_PROJECT_URL ?? 'http://your.supabase.project.url',
+        import.meta.env.NG_APP_SUPABASE_PUBLISHABLE_KEY ?? 'your-supabase-publishable-key',
         { auth: { throwOnError: true } },
       ),
     providedIn: 'root',
