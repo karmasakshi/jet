a8e73d6cb97909e68821f096df205e95026ba2ae
karmasakshi <karmasakshi@users.noreply.github.com>
2026-06-26 22:33:18 +0530

    chore(general): clean-up

--- src/app/enums/supabase-table.enum.ts
+++ src/app/enums/supabase-table.enum.ts
@@ -1,7 +1,5 @@
 export enum SupabaseTable {
-  AppPermissions = 'app_permissions',
   AppPermissionsAppRoles = 'app_permissions_app_roles',
-  AppRoles = 'app_roles',
   AppRolesUsers = 'app_roles_users',
   Profiles = 'profiles',
 }
