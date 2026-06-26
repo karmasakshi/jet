01ea9c2b2e3a15534b9208c1cd0a2b4446327dfd
karmasakshi <karmasakshi@users.noreply.github.com>
2026-06-27 02:28:18 +0530

    chore(general): clean-up

--- supabase/migrations/10_storage_rls_policies.sql
+++ supabase/migrations/10_storage_rls_policies.sql
@@ -1,11 +1,11 @@
 -- profile_avatars
 
-create policy "Allow public to select any" on storage.objects
+create policy "profile_avatars: Allow public to select any" on storage.objects
 for select
 to public
 using (bucket_id = 'profile_avatars');
 
-create policy "Allow authenticated to insert in own folder" on storage.objects
+create policy "profile_avatars: Allow authenticated to insert in own folder" on storage.objects
 for insert
 to authenticated
 with check (
@@ -13,7 +13,7 @@ with check (
   and (select auth.uid())::text = (storage.foldername(name))[1]
 );
 
-create policy "Allow authenticated to update in own folder" on storage.objects
+create policy "profile_avatars: Allow authenticated to update in own folder" on storage.objects
 for update
 to authenticated
 using (bucket_id = 'profile_avatars' and (select auth.uid())::text = (storage.foldername(name))[1])
@@ -22,7 +22,7 @@ with check (
   and (select auth.uid())::text = (storage.foldername(name))[1]
 );
 
-create policy "Allow authenticated to delete in own folder" on storage.objects
+create policy "profile_avatars: Allow authenticated to delete in own folder" on storage.objects
 for delete
 to authenticated
 using (bucket_id = 'profile_avatars' and (select auth.uid())::text = (storage.foldername(name))[1]);
